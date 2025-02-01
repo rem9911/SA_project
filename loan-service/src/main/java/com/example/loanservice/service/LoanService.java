d3d6f1a6facf60622bda2819e74ec4e38b19fff7
BiancaDeara <biancadeara1308@gmail.com>
2025-02-01 12:43:17 +0200

    added databaseconfig for loan service

--- loan-service/src/main/java/com/example/loanservice/service/LoanService.java
+++ loan-service/src/main/java/com/example/loanservice/service/LoanService.java
@@ -1,36 +1,29 @@
-package com.example.loanservice.controller;
+package com.example.loanservice.service;
+
 
 import com.example.loanservice.kafka.LoanProducer;
 import com.example.loanservice.model.Loan;
 import com.example.loanservice.repository.LoanRepository;
-import com.example.loanservice.service.LoanService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/loans")
-public class LoanController {
+@Service
+public class LoanService {
 
     private final LoanRepository loanRepository;
     private final LoanProducer loanProducer;
-    @Autowired
-    public LoanService loanService;
-
-
-    public LoanController(LoanRepository loanRepository, LoanProducer loanProducer) {
-        this.loanRepository = loanRepository;
-        this.loanProducer = loanProducer;
-    }
 
     @GetMapping
     public List<Loan> getAllLoans() {
         return loanRepository.findAll();
     }
 
-    @PostMapping
+
     public Loan createLoan(@RequestBody Loan loan) {
         // Assure-toi que l'objet reçu est bien traité avant de l'enregistrer
         Loan savedLoan = loanRepository.save(loan);
